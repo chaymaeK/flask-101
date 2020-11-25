92b23fad426a194c93b053b9b0735afa3acc9b05
kANDOUSSI Chaymae <chaymae.kandoussi@bnpparibas.com>
2020-11-25 22:22:38 +0100

    test 1 OK

--- wsgi.py
+++ wsgi.py
@@ -15,5 +15,6 @@ def list_products():
     PRODUCTS = {
     1: { 'id': 1, 'name': 'Skello' },
     2: { 'id': 2, 'name': 'Socialive.tv' },
+    3: { 'id': 3, 'name': 'test'}
     }
     return jsonify(PRODUCTS)
