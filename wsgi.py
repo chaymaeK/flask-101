ae2cd6adabfe3ae4608fbe9fe0032e80a8943a49
kANDOUSSI Chaymae <chaymae.kandoussi@bnpparibas.com>
2020-11-26 21:35:41 +0100

    correction

--- wsgi.py
+++ wsgi.py
@@ -37,3 +37,48 @@ def delete_one_product(id):
 
     return '', 202
 
+
+@app.route(f'{BASE_URL}/products', methods=['POST'])
+def create_one_product():
+    data = request.get_json()
+
+    if data is None:
+        abort(400)
+
+    name = data.get('name')
+
+    if name is None:
+        abort(400)
+
+    if name == '' or not isinstance(name, str):
+        abort(422)
+
+    next_id = next(IDENTIFIER_GENERATOR)
+    new_product = {'id' : next_id , 'name' : name }
+    PRODUCTS[next_id] = new_product #{'id' : next_id , 'name' : name }
+
+    return jsonify(new_product), 201 #PRODUCTS[next_id]), 201
+
+
+@app.route(f'{BASE_URL}/products/<int:product_id>', methods=['PATCH'])
+def update_one_product(product_id):
+    data = request.get_json()
+    if data is None:
+        abort(400)
+
+    name = data.get('name')
+
+    if name is None:
+        abort(400)
+
+    if name == '' or not isinstance(name, str):
+        abort(422)
+
+    product = PRODUCTS.get(product_id)
+
+    if product is None:
+        abort(404)
+
+    PRODUCTS[product_id]['name'] = name
+
+    return None, 204
